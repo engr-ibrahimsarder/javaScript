a2829c14250dcda10eabb0f7113b7a573c48e1f4
unknown <ibrahimsarder969@gmail.com>
2024-12-15 16:34:01 +0600

    function add here normal anonymous arrow

--- function.js
+++ function.js
@@ -1,9 +1,61 @@
 // normal function
+    
+// function sum(x, y){
+// return 0;
+// };
+// let a = 5;
+// let b = 7;
+// let total = sum(a, b);
+// console.log("total:", total);
 
-function sum(x, y){
-return 0;
+// division
+// function division(x,y){
+//     let total=x/y;
+//     return total;
+// }
+// let c =12;
+// let d = 4;
+// let total= division(c,d);
+// console.log(total);
+
+// function subtraction(x,y){
+//     return(x-y);
+// }
+// let e = 50;
+// let f = 10;
+// let totalsubtraction = subtraction(e,f);
+// console.log(totalsubtraction);
+
+// function fullName(firstName, lastName, age){
+//     return 'Your first Name' + ' ' + firstName + ' ' + 'last Name'+ ' ' + lastName + '. Your age'+ ' ' + age;
+// }  
+
+// const yourName = fullName('abir', 'ahmed' , 22);
+// console.log(yourName);
+  
+function gold(ana){
+    let vori = ana/16;
+    return `64 ana = ${vori} vori`;
+}
+// let output = gold(64);
+// console.log(output)
+// user value 64.
+// output: 64 ana = 4 vori
+// user value 10000
+// output: 10000 metter =  Kilometer
+
+// anonymous function
+// let sum = function(num1, num2){
+//     return num1 + num2;
+// };
+
+// arrow function
+// let sum = (num1, num2) => num1 + num2;
+let sum = (num1, num2) => {
+    let total = num1 + num2;
+    return total * total;
 };
-let a = 5;
-let b = 7;
-let total = sum(a, b);
-console.log("line 11:", total);
+console.log(sum(10, 20));
+
+
+
