d16253e12a5ad942cc83c869ebfb931a001d2b5b
unknown <ibrahimsarder969@gmail.com>
2024-12-18 16:40:50 +0600

    string method add

---
+++ string.js
@@ -0,0 +1,28 @@
+// string startsWith method
+// let str = 'I love my country';
+// let convert = str.toLowerCase();
+// let len = convert.startsWith('i');
+// console.log(len); 
+// string slice method
+// let fruits = "apple, mango, orange"; 
+// let slc = fruits.slice(8, 11);
+// console.log(slc);
+// string replace method
+// let fruits = "apple, mango, orange"; 
+// let rep = fruits.replace('orange', 'cherry');
+// console.log(rep);
+// string split method 
+/*let fruits = "apple,  mango, orange"; 
+let rep = fruits.split('');
+console.log(rep);*/
+// string trim method 
+/*let str = "       I love my country"; 
+let trm = str.trim();
+
+console.log(trm.length);
+console.log(rep);*/
+// string repeat method 
+let str = "I love my country";
+let rept = str.repeat(3);
+
+console.log(rept);
